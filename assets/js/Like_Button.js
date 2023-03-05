1981e1fd18fef138ef8e54e9af94f0a06ad4387e
Samson Safo <95703627+Safo-Samson@users.noreply.github.com>
2023-03-05 22:45:01 +0000

    tested React, added dropdown pages, and test page

---
+++ assets/js/Like_Button.js
@@ -0,0 +1,22 @@
+
+'use strict';
+
+const e = React.createElement;
+
+function LikeButton(props) {
+    const [liked, setLiked] = React.useState(false)
+
+    if (liked) {
+        return 'You liked this.';
+    }
+
+    return e(
+        'button',
+        { onClick: () => setLiked(true) },
+        'Like'
+    );
+}
+
+const domContainer = document.querySelector('#like_button_container');
+const root = ReactDOM.createRoot(domContainer);
+root.render(e(LikeButton));
