65f39b6a1d5178c72fac45d2dc0295e4bbc714f3
Samson Safo <95703627+Safo-Samson@users.noreply.github.com>
2023-03-08 21:06:10 +0000

    login and sign up

--- assets/js/logins.js
+++ assets/js/logins.js
@@ -1,20 +1,20 @@
 
 
-let modal = document.getElementById('id01');
+let modalSignUp = document.getElementById('id01');
 
 // When the user clicks anywhere outside of the modal, close it
 window.onclick = function (event) {
-    if (event.target == modal) {
-        modal.style.display = "none";
+    if (event.target === modal) {
+        modalSignUp.style.display = "none";
     }
 }
 
-// Get the modal
+// Get the login modal
 let modalLogin = document.getElementById('id02');
 
 // When the user clicks anywhere outside of the modal, close it
 window.onclick = function (event) {
-    if (event.target == modalLogin) {
+    if (event.target === modalLogin) {
         modalLogin.style.display = "none";
     }
 }
