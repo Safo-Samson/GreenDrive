afd0f6b337ff01e81c6feac45bba1060ab01451a
Samson Safo <95703627+Safo-Samson@users.noreply.github.com>
2023-03-10 16:18:19 +0000

    touches to news page

--- assets/js/SlideShow.js
+++ assets/js/SlideShow.js
@@ -22,8 +22,9 @@ function showSlides(n) {
         slides[i].style.display = "none";
     }
     for (i = 0; i < dots.length; i++) {
-        dots[i].className = dots[i].className.replace(" active", "");
+        dots[i].className = dots[i].className.replace(" dot-active", "");
     }
     slides[slideIndex - 1].style.display = "block";
-    dots[slideIndex - 1].className += " active";
+    dots[slideIndex - 1].className += " dot-active";
 }
+
