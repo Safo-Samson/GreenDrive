336700ce987de61ce30de559689aed2ac79c6acc
Samson Safo <95703627+Safo-Samson@users.noreply.github.com>
2023-03-13 12:00:30 +0000

    news page done

--- assets/js/SlideShow.js
+++ assets/js/SlideShow.js
@@ -12,19 +12,64 @@ function currentSlide(n) {
     showSlides(slideIndex = n);
 }
 
+let slideshowTimeout;
+
 function showSlides(n) {
     let i;
-    let slides = document.getElementsByClassName("mySlides");
-    let dots = document.getElementsByClassName("dot");
+    let slides = document.getElementsByClassName("Green-mySlides");
+    let dots = document.getElementsByClassName("Green-dot");
     if (n > slides.length) { slideIndex = 1 }
     if (n < 1) { slideIndex = slides.length }
     for (i = 0; i < slides.length; i++) {
         slides[i].style.display = "none";
     }
     for (i = 0; i < dots.length; i++) {
-        dots[i].className = dots[i].className.replace(" dot-active", "");
+        dots[i].className = dots[i].className.replace(" Green-active", "");
     }
     slides[slideIndex - 1].style.display = "block";
-    dots[slideIndex - 1].className += " dot-active";
+    dots[slideIndex - 1].className += " Green-active";
+
+    // Clear the timeout when the mouse is over the current slide
+    let currentSlide = slides[slideIndex - 1];
+    currentSlide.addEventListener("mouseover", function () {
+        clearTimeout(slideshowTimeout);
+    });
+
+    // Set the timeout again when the mouse moves away from the current slide
+    currentSlide.addEventListener("mouseout", function () {
+        slideshowTimeout = setTimeout(function () {
+            plusSlides(1);
+        }, 2000);
+    });
+
+    // Clear the timeout when the mouse is over the previous/next symbols
+    let prevSymbol = document.getElementsByClassName("Green-prev")[0];
+    let nextSymbol = document.getElementsByClassName("Green-next")[0];
+    prevSymbol.addEventListener("mouseover", function () {
+        clearTimeout(slideshowTimeout);
+    });
+    nextSymbol.addEventListener("mouseover", function () {
+        clearTimeout(slideshowTimeout);
+    });
+
+    // Set the timeout again when the mouse moves away from the previous/next symbols
+    prevSymbol.addEventListener("mouseout", function () {
+        slideshowTimeout = setTimeout(function () {
+            plusSlides(-1);
+        }, 2000);
+    });
+    nextSymbol.addEventListener("mouseout", function () {
+        slideshowTimeout = setTimeout(function () {
+            plusSlides(1);
+        }, 2000);
+    });
+}
+
+function plusSlides(n) {
+    clearTimeout(slideshowTimeout); // clear the timeout when the symbols are clicked
+    showSlides(slideIndex += n);
+    slideshowTimeout = setTimeout(function () {
+        plusSlides(1);
+    }, 2000);
 }
 
