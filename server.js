48212154c36ad3b3fcabb8b32ba847b3a16c6918
Joseph halden <josephhalden1@gmail.com>
2023-04-05 23:31:28 +0100

    edited server.js
    our jwt secret is jwt_secret

--- server.js
+++ server.js
@@ -5,22 +5,55 @@ const MongoStore = require('connect-mongo');
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 
+const connect = require('./db');
+const User = require('./models/User');
+
 const app = express();
 const port = process.env.PORT || 3001;
 
 // Serve static files
+app.use(express.json());
 app.use(express.static(path.join(__dirname)));
 
 // Session configuration
 app.use(
   session({
-    secret: Greendrive, // replace this with your own secret key
+    secret: 'Greendrive', // replace this with your own secret key
     resave: false,
     saveUninitialized: false,
-    store: MongoStore.create({ mongoUrl: 'mongodb+srv://Greendrive2023:Greendrive123@cluster0.poflabt.mongodb.net/your_database_name?retryWrites=true&w=majority' }),
+    store: MongoStore.create({ mongoUrl: 'mongodb+srv://Greendrive2023:Greendrive123@cluster0.poflabt.mongodb.net/freendrive_db?retryWrites=true&w=majority' }),
     cookie: { maxAge: 86400000 },
   })
 );
+
+app.post('/register', async (req, res) => {
+  const { email, password, userType } = req.body;
+  const db = await connect();
+  const userModel = new User(db);
+
+  const existingUser = await userModel.getUserByEmail(email);
+
+  if (existingUser) {
+    return res.status(400).json({ message: 'User already exists' });
+  }
+
+  const hashedPassword = await bcrypt.hash(password, 10);
+  const newUser = {
+    email,
+    password: hashedPassword,
+    userType,
+  };
+
+  const createdUser = await userModel.createUser(newUser);
+  delete createdUser.password;
+
+  const token = jwt.sign({ id: createdUser._id }, 'jwt_secret', {
+    expiresIn: '1d',
+  });
+
+  req.session.token = token;
+  res.status(201).json(createdUser);
+});
 
 // Catch-all route to serve the main HTML file
 app.get('*', (req, res) => {
