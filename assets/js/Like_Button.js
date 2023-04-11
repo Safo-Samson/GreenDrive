d06847c3f26cdfe1e911adab0cb6054072aa0e4b
Samson Safo <95703627+Safo-Samson@users.noreply.github.com>
2023-04-11 11:52:40 +0100

    styles for each person

--- assets/js/Like_Button.js
+++ assets/js/Like_Button.js
@@ -1,4 +1,26 @@
 
+// 'use strict';
+
+// const e = React.createElement;
+
+// function LikeButton(props) {
+//     const [liked, setLiked] = React.useState(false)
+
+//     if (liked) {
+//         return 'You liked this.';
+//     }
+
+//     return e(
+//         'button',
+//         { onClick: () => setLiked(true) },
+//         'Like'
+//     );
+// }
+
+// const domContainer = document.querySelector('#like_button_container');
+// const root = ReactDOM.createRoot(domContainer);
+// root.render(e(LikeButton));
+
 'use strict';
 
 const e = React.createElement;
@@ -6,17 +28,26 @@ const e = React.createElement;
 function LikeButton(props) {
     const [liked, setLiked] = React.useState(false)
 
-    if (liked) {
-        return 'You liked this.';
+    const buttonStyle = {
+        backgroundColor: 'red',
+        color: 'green',
+        fontSize: '20px',
+        padding: '10px',
+        borderRadius: '5px',
+        border: 'none',
+        cursor: 'pointer',
+        margin: '10px'
     }
 
-    return e(
-        'button',
-        { onClick: () => setLiked(true) },
-        'Like'
+    if (liked) {
+        return <p style={{ color: 'green' }}>You liked this.</p>;
+    }
+
+    return (
+        <button style={buttonStyle} onClick={() => setLiked(true)}>Like</button>
     );
 }
 
-const domContainer = document.querySelector('#like_button_container');
+const domContainer = document.querySelector('#like_Button');
 const root = ReactDOM.createRoot(domContainer);
-root.render(e(LikeButton));
+root.render(<LikeButton />);
