ac57f29f6f46e1afd1d7333af7dada5f2e030510
Samson Safo <95703627+Safo-Samson@users.noreply.github.com>
2023-04-13 13:47:31 +0100

    news API done

---
+++ assets/js/hybrid-news.js
@@ -0,0 +1,72 @@
+const apiKey = "748b841297bc487b9bf6c0bfeb1ba1dd";
+const countryCode = "us";
+const category = " technology";
+const keywords = "hybrid vehicles";
+// vehicles, cars, automobiles, fuel, gasoline, diesel, electric vehicles, EVs, hybrid vehicles,
+const pageSize = "20";
+const images = ["/assets/img/news1.jpg", "/assets/img/news2.jpg", "/assets/img/news3.jpg", "/assets/img/news4.jpg", "/assets/img/news5.jpg",
+    "/assets/img/news6.jpg", "/assets/img/news7.jpg", "/assets/img/news9.jpg", "/assets/img/news10.jpg"]; //hard coding images because the article.urlToImage is paid
+
+// const url = `https://newsapi.org/v2/top-headlines?country=${countryCode}&category=${category}&pageSize=${pageSize}&apiKey=${apiKey}&pageSize=20&language=en&sortBy=publishedAt`;
+const url = `https://newsapi.org/v2/everything?q=${keywords}&pageSize=${pageSize}&apiKey=${apiKey}`;
+fetch(url)
+    .then(response => response.json())
+    .then(data => {
+        const newsContainer = document.getElementById("hybrid-news-container");
+
+        // Get the first 5 articles
+        const firstFiveArticles = data.articles.slice(0, 5);
+
+        // Loop through the first 5 articles and create a new element for each one
+        firstFiveArticles.forEach(article => {
+
+            const articleElement = document.createElement("div");
+
+            const titleElement = document.createElement("h2");
+            titleElement.textContent = article.title;
+
+            const descriptionElement = document.createElement("p");
+            descriptionElement.textContent = article.description;
+            console.log(article.description)
+
+            const imageElement = document.createElement("img");
+            // imageElement.src = article.urlToImage; this doesn't return anything because the NewsAPI is free for now
+            const randomIndex = Math.floor(Math.random() * images.length);
+            imageElement.src = images[randomIndex];
+            imageElement.classList.add("news-image"); //to add the style which will make them equal
+
+            const contentElement = document.createElement("p");
+            contentElement.textContent = article.content;
+
+            // Extract the first 7 sentences of the article   --- doesn't work because current version is free
+            const firstSevenSentences = article.content;
+
+            const previewElement = document.createElement("p");
+            previewElement.textContent = `${firstSevenSentences}...`;
+
+            const linkElement = document.createElement("a");
+            linkElement.href = article.url;
+            linkElement.target = '_blank'
+            linkElement.textContent = "Read more";
+            linkElement.classList.add('green-blink')
+
+            // Apply black color to the paragraphs
+            titleElement.style.color = "black";
+            descriptionElement.style.color = "black";
+            previewElement.style.color = "black";
+
+
+            // Add the elements to the article container
+            articleElement.appendChild(titleElement);
+            articleElement.appendChild(descriptionElement);
+            articleElement.appendChild(imageElement);
+            articleElement.appendChild(previewElement);
+            articleElement.appendChild(linkElement);
+
+            // Add the article container to the news container
+            newsContainer.appendChild(articleElement);
+        });
+    })
+    .catch(error => {
+        console.log(error);
+    });
