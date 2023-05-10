eccc153e1b31cdd9914998796cb387d9b3961773
Samson Safo <95703627+Safo-Samson@users.noreply.github.com>
2023-05-10 11:37:14 +0100

    minor on styles

--- backend/vehicle-enquiry.js
+++ backend/vehicle-enquiry.js
@@ -4,7 +4,6 @@ const https = require('https');
 const { parse } = require('querystring');
 
 const apiKey = 'mDKpeAwFn72aQKOS8cBxV5KB9hvfjVMD8NRLCmtP';
-
 const options = {
     hostname: 'driver-vehicle-licensing.api.gov.uk',
     path: '/vehicle-enquiry/v1/vehicles',
@@ -14,32 +13,29 @@ const options = {
         'x-api-key': apiKey,
     },
 };
-
 const handleDVLARequest = (req, res) => {
     if (req.method === 'GET' && req.url === '/backend/vehicle-enquiry.js') {
         let rawData = '';
-
         req.on('data', (chunk) => {
             rawData += chunk;
         });
-
         req.on('end', () => {
             try {
                 const { registrationNumber } = JSON.parse(rawData);
                 const data = JSON.stringify({ registrationNumber });
-
                 const req = https.request(options, (apiRes) => {
                     console.log(`statusCode: ${apiRes.statusCode}`);
                     let apiData = '';
                     apiRes.on('data', (chunk) => {
                         apiData += chunk;
                     });
-
                     apiRes.on('end', () => {
                         try {
                             const vehicleData = JSON.parse(apiData);
                             const { make, colour, yearOfManufacture } = vehicleData;
                             console.log(`Make: ${make}, Colour: ${colour}, Year of Manufacture: ${yearOfManufacture}`);
+
+                            // <div id="make-heading">${make}</div>
 
                             res.writeHead(200, { 'Content-Type': 'application/json' });
                             res.end(JSON.stringify({ make, colour, yearOfManufacture }));
@@ -49,11 +45,9 @@ const handleDVLARequest = (req, res) => {
                         }
                     });
                 });
-
                 req.on('error', (error) => {
                     console.error(error);
                 });
-
                 req.write(data);
                 req.end();
 
